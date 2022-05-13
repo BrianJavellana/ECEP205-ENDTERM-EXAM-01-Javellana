68c08d733fcd16de2847a12b949d3c6f0363eb5a
BrianJavellana <s1820421@usls.edu.ph>
2022-05-13 11:52:28 +0800

    add OrderTest.java

---
+++ src/com/ecpe205/OrderTest.java
@@ -0,0 +1,11 @@
+package com.ecpe205;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+    @Test
+    public void shouldItemEqualtoExpectedValue(){}
+
+}
